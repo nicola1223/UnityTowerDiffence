089d7e68bd926d69ecaedb210198397dbc0e1c9f
nicola1223 <belchuk2005@mail.ru>
2021-03-12 18:29:08 +0300

    first commit

--- Assets/Scripts/Camera/Mouse.cs
+++ Assets/Scripts/Camera/Mouse.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class Mouse : MonoBehaviour
 {
-
+	public bool InvertX = false;
+	public bool InvertY = false;
+	public bool Invert_X_to_Y = false; 
 	public int speedM = 10;
 	public int otstup = 50;
+	private int xinv = 1;
+	private int yinv = 1;
 	// Start is called before the first frame update
     void Start()
     {
-        
+    	if (InvertX)
+    	{
+    		xinv = -1;	
+    	} 
+    	else
+    	{
+    		xinv = 1;
+    	}
+    	if (InvertY)
+    	{
+    		yinv = -1;	
+    	} 
+    	else
+    	{
+    		yinv = 1;
+    	}
     }
 
     // Update is called once per frame
@@ -20,24 +39,52 @@ public class Mouse : MonoBehaviour
 
         if ((Input.mousePosition.x > Screen.width - otstup) && (Input.mousePosition.x <= Screen.width))
         {
-     		var speed = speedM / scale(0, otstup, 0, 5, (Screen.width - Input.mousePosition.x));
-        	transform.Translate(Vector3.right * speed * Time.fixedDeltaTime, Space.World);
+        	var speed = speedM / scale(0, otstup, 0, 5, (Screen.width - Input.mousePosition.x));
+        	if (Invert_X_to_Y)
+        	{
+        		transform.Translate(Vector3.forward * yinv * speed * Time.fixedDeltaTime, Space.World);
+        	}
+        	else
+        	{
+        		transform.Translate(Vector3.right * xinv * speed * Time.fixedDeltaTime, Space.World);
+        	}
         }
         else if ((Input.mousePosition.x < otstup) && (Input.mousePosition.x >= 0))
         {
         	var speed = speedM / scale(0, otstup, 0, 5, Input.mousePosition.x);
-        	transform.Translate(Vector3.left * speed * Time.fixedDeltaTime, Space.World);
+        	if (Invert_X_to_Y)
+        	{
+        		transform.Translate(Vector3.back * speed * yinv * Time.fixedDeltaTime, Space.World);
+        	}
+        	else
+        	{
+        		transform.Translate(Vector3.left * xinv * speed * Time.fixedDeltaTime, Space.World);
+        	}
         }
 
         if ((Input.mousePosition.y > Screen.height - otstup) && (Input.mousePosition.y <= Screen.height))
         {
         	var speed = speedM / scale(0, otstup, 0, 5, (Screen.height - Input.mousePosition.y));
-        	transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime, Space.World);
+        	if (Invert_X_to_Y)
+        	{
+        		transform.Translate(Vector3.right * xinv * speed * Time.fixedDeltaTime, Space.World);
+        	}
+        	else
+        	{
+        		transform.Translate(Vector3.forward * speed * yinv * Time.fixedDeltaTime, Space.World);
+        	}
         }
         else if ((Input.mousePosition.y < otstup) && (Input.mousePosition.y >= 0))
         {
         	var speed = speedM / scale(0, otstup, 0, 5, Input.mousePosition.y);
-        	transform.Translate(Vector3.back * speed * Time.fixedDeltaTime, Space.World);
+        	if (Invert_X_to_Y)
+        	{
+        		transform.Translate(Vector3.left * xinv * speed * Time.fixedDeltaTime, Space.World);
+        	}
+        	else
+        	{
+        		transform.Translate(Vector3.back * speed * yinv * Time.fixedDeltaTime, Space.World);
+        	}
         }
     }
 
